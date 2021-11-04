65eb2e0d26a1566ce16fe8df06919e625f2678af
Vanessa Santana <vanessah4money@gmail.com>
2021-11-03 23:20:54 -0300

    feat|: comecando a entender osmiddlewares

--- aula-05/script.js
+++ aula-05/script.js
@@ -12,3 +12,23 @@ const getAttrId = getElementAttr("id");
 
 const dataSlideList = li.map(getAttrDataSlide);
 const idList = li.map(getAttrId);
+
+//Middleware
+
+function reducer(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENTAR':
+            return state + 1;
+        case 'REDUZIR':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+const logger = (store) => (next) => (action) => {
+    return next(action)
+}
+
+const middleware = Redux.applyMiddleware(logger);
+const store = Redux.createStore(reducer, middleware);
