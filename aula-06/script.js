5deb6c68fd21140089274382429159aa6292497b
Vanessa Santana <vanessah4money@gmail.com>
2021-11-12 16:11:52 -0300

    feat: salvando no localStorage

--- aula-06/script.js
+++ aula-06/script.js
@@ -5,9 +5,16 @@
 // o dispatch como argumento da mesma.
 // https://stackoverflow.com/questions/34570758/why-do-we-need-middleware-for-async-flow-in-redux
 
+function getLocalStorage(key, initial) {
+  try {
+    return JSON.parse(window.localStorage.getItem(key));
+  } catch(error) {
+    return initial
+  }
+}
 const initialState = {
     loading: false,
-    data: null,
+    data: getLocalStorage('data', null),
     error: null
 };
 
@@ -19,9 +26,17 @@ const thunk = (store) => (next) => (action) => {
     }
 };
 
+const localStorage = (store) => (next) => (action) => {
+  const result = next(action);
+  if (action.localStorage !== undefined) {
+    window.localStorage.setItem(action.localStorage, JSON.stringify(action.payload))
+  }
+  return result;
+}
+
 const { compose, applyMiddleware } = Redux;
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhancer = composeEnhancers(applyMiddleware(thunk));
+const enhancer = composeEnhancers(applyMiddleware(thunk, localStorage));
 const store = Redux.createStore(reducer, enhancer);
 
 function reducer (state = initialState, action) {
@@ -42,11 +57,15 @@ function fetchUrl(url) {
       try {
         dispatch({ type: 'FETCH_STARTED' });
         const data = await fetch(url).then((r) => r.json());
-        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data, localStorage: 'data' });
       } catch (error) {
         dispatch({ type: 'FETCH_ERROR', payload: error.message });
       }
     };
 }
-  
-store.dispatch(fetchUrl('https://dogsapi.origamid.dev/json/api/photo'));
+
+const state = store.getState();
+if (state.data == null) {
+  store.dispatch(fetchUrl('https://dogsapi.origamid.dev/json/api/photo'));
+}
+
